e31927161a4644752ebd51d5d32dd4bf24251ed5
Milesq <wmilosz88@gmail.com>
2022-06-22 00:15:14 +0200

    feat: display tab pane with places

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,29 @@
 <template>
-  <el-calendar v-model="value" />
+  <section class="px-10">
+    <el-tabs v-if="places?.length" v-model="activePlace" class="demo-tabs">
+      <el-tab-pane label="Mój kalendarz" name="my-calendar"></el-tab-pane>
+      <el-tab-pane
+        v-for="place in places"
+        :key="place.id"
+        :label="place.name"
+        :name="place.name"
+      >
+        {{ place.name }}
+      </el-tab-pane>
+    </el-tabs>
+  </section>
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue'
-const value = ref(new Date())
+import { onMounted, ref } from 'vue'
+import { DataStore } from '@aws-amplify/datastore'
+import { Place } from '@/models'
+
+const places = ref<Place[]>()
+
+const activePlace = ref('my-calendar')
+
+onMounted(async () => {
+  places.value = await DataStore.query(Place)
+})
 </script>
