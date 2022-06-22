5edbfec8915cf6ec93e16ecaf6881802cf070340
Milesq <wmilosz88@gmail.com>
2022-06-22 17:55:36 +0200

    feat: pass only proper appointments to `ShowAppointment`

--- src/components/ShowAppointments.vue
+++ src/components/ShowAppointments.vue
@@ -1,14 +1,9 @@
 <script setup lang="ts">
 import type { Appointment } from '@/models'
-import { onMounted } from 'vue'
 
-const props = defineProps<{
+defineProps<{
   appointments: Appointment[]
 }>()
-
-onMounted(() => {
-  console.log(props.appointments)
-})
 </script>
 
 <template>
