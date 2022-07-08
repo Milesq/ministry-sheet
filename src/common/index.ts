4533a97bfd01e0233a7dba9349ba160a964624ba
Milesq <wmilosz88@gmail.com>
2022-07-08 17:55:12 +0200

    feat: `AppointmentWithUsers`

--- src/common/index.ts
+++ src/common/index.ts
@@ -1,2 +1,3 @@
 export type { default as CalendarEvent } from './CalendarEvent'
+export type { default as AppointmentWithUsers } from './AppointmentWithUsers'
 export { default as getAppointmentUsers } from './getAppointmentUsers'
