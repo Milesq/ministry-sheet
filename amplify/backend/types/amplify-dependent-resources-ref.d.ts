f3c2ed76b8a10b75fd3878423e469872454963b2
Milesq <wmilosz88@gmail.com>
2022-07-22 20:18:50 +0200

    set defaualt auth method to cognito user group

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,7 +1,6 @@
 export type AmplifyDependentResourcesAttributes = {
     "api": {
         "ministrysheet": {
-            "GraphQLAPIKeyOutput": "string",
             "GraphQLAPIIdOutput": "string",
             "GraphQLAPIEndpointOutput": "string"
         },
