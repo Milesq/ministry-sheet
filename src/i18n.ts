8dfd0f04f7ef5e4ac22a702f279901eae23de569
Milesq <wmilosz88@gmail.com>
2022-07-24 20:35:37 +0200

    basic vue-i18n usage

---
+++ src/i18n.ts
@@ -0,0 +1,16 @@
+import { createI18n } from 'vue-i18n'
+
+import pl from './locales/pl.json'
+import en from './locales/en.json'
+
+const i18n = createI18n({
+  legacy: false,
+  globalInjection: true,
+  locale: localStorage.getItem('lang') || navigator.language || 'pl',
+  messages: {
+    en,
+    pl,
+  },
+})
+
+export default i18n
