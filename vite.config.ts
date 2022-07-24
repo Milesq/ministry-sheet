e9ad9b86b6bf87ed1277088fdd4740b740989333
Milesq <wmilosz88@gmail.com>
2022-07-24 20:45:38 +0200

    setup tree shaking

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import vue from '@vitejs/plugin-vue'
 import Icons from 'unplugin-icons/vite'
 import WindiCSS from 'vite-plugin-windicss'
+import { vueI18n } from '@intlify/vite-plugin-vue-i18n'
+import { resolve, dirname } from 'node:path'
 import AutoImport from 'unplugin-auto-import/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import Components from 'unplugin-vue-components/vite'
@@ -10,6 +12,12 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   plugins: [
     vue(),
+    vueI18n({
+      include: resolve(
+        dirname(fileURLToPath(import.meta.url)),
+        './src/locales/**'
+      ),
+    }),
     Icons({ compiler: 'vue3' }),
     AutoImport({
       resolvers: [ElementPlusResolver()],
