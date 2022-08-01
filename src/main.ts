882cd36f64c92a7106498ac5a19f098494a77488
Milesq <wmilosz88@gmail.com>
2022-08-01 21:54:21 +0200

    remove element+ locale

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import { Auth } from '@aws-amplify/auth'
 import { createPinia } from 'pinia'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import ElementPlus from 'element-plus'
-import pl from 'element-plus/es/locale/lang/pl'
 import 'element-plus/dist/index.css'
 import 'element-plus/theme-chalk/dark/css-vars.css'
 import 'virtual:windi.css'
@@ -34,9 +33,7 @@ app.use(pinia)
 
 app.use(router)
 
-app.use(ElementPlus, {
-  locale: pl,
-})
+app.use(ElementPlus)
 
 app.use(i18n)
 
