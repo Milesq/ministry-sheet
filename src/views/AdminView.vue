a1e97134ea4906414a5904e18a3ad8bfc916f61a
Milesq <wmilosz88@gmail.com>
2022-08-15 13:57:20 +0200

    feat: show cal events on admin view

--- src/views/AdminView.vue
+++ src/views/AdminView.vue
@@ -1,3 +1,60 @@
+<script lang="ts" setup>
+import { computed, onMounted, ref, watchEffect } from 'vue'
+import type { Appointment, PendingAppointment, Place } from '@/models'
+import useAppointments from '@/stores/appointments'
+import { makeCalEvents } from '@/common'
+
+const activePlace = ref('my-calendar')
+const appointments = useAppointments()
+
+onMounted(() => {
+  appointments.init()
+})
+
+watchEffect(() => {
+  if (appointments.places.length)
+    activePlace.value = appointments.places[0].name
+})
+
+const events = computed(() => {
+  const place = appointments.places.find(
+    ({ name }) => name === activePlace.value
+  )
+
+  if (!place) return []
+
+  return makeCalEvents({
+    appointments: getAppointments(place),
+    pendingAppointments: getPendings(place),
+  })
+})
+
+function getAppointments({ id }: Place): Appointment[] {
+  return appointments.appointments?.filter(({ place }) => place.id === id)
+}
+
+function getPendings({ id }: Place): PendingAppointment[] {
+  return appointments.pendingAppointments?.filter(
+    ({ place }) => place.id === id
+  )
+}
+</script>
+
 <template>
-  <div>Hi admin</div>
+  <section class="px-10">
+    <el-tabs
+      v-if="appointments.places?.length"
+      v-model="activePlace"
+      class="demo-tabs"
+    >
+      <el-tab-pane
+        v-for="place in appointments.places"
+        :key="place.id"
+        :label="place.name"
+        :name="place.name"
+      >
+        <AppCalendar v-if="activePlace === place.name" :events="events" />
+      </el-tab-pane>
+    </el-tabs>
+  </section>
 </template>
