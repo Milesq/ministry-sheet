338327ec18be7f24b35aa662c3db6e26334412db
Milesq <wmilosz88@gmail.com>
2022-08-27 16:28:59 +0200

    feat: place editor

--- src/views/admin/Places.vue
+++ src/views/admin/Places.vue
@@ -1,5 +1,29 @@
+<script lang="ts" setup>
+import { computed } from 'vue'
+import pick from 'lodash.pick'
+import useAppointments from '@/stores/appointments'
+import { DataStore } from '@aws-amplify/datastore'
+import { Place } from '@/models'
+
+import EditableList, { type Item } from '../../components/EditableList.vue'
+
+const appointments = useAppointments()
+
+const places = computed(() => {
+  return appointments.places.map(place => pick(place, ['id', 'name'])) as Item[]
+})
+
+function addPlace(name: string) {
+  DataStore.save(new Place({ name }))
+}
+
+async function deletePlace(id: string) {
+  const place = await DataStore.query(Place, id)
+
+  if (place) await DataStore.delete(place)
+}
+</script>
+
 <template>
-  <div>
-    ok
-  </div>
+  <EditableList :items="places" @add="addPlace" @delete="deletePlace" />
 </template>
