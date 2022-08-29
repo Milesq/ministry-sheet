e630851c1b8ab5d87454b29c48be94ac260057de
Milesq <wmilosz88@gmail.com>
2022-08-29 22:24:33 +0200

    feat: add min place name length

--- src/views/admin/Places.vue
+++ src/views/admin/Places.vue
@@ -1,19 +1,31 @@
 <script lang="ts" setup>
 import { computed } from 'vue'
+import { useI18n } from 'vue-i18n'
 import pick from 'lodash.pick'
 import useAppointments from '@/stores/appointments'
 import { DataStore } from '@aws-amplify/datastore'
 import { Place } from '@/models'
+import { Swal } from '@/common'
 
 import EditableList, { type Item } from '../../components/EditableList.vue'
 
 const appointments = useAppointments()
+const { t } = useI18n()
 
 const places = computed(() => {
   return appointments.places.map(place => pick(place, ['id', 'name'])) as Item[]
 })
 
+function checkPlaceName(name: string) {
+  if (name.length < 3) {
+    Swal.fire(t('error.err'), t('error.placeNameTooShort', [3]), 'error')
+
+    throw new Error('Place name must be at least 3 characters long')
+  }
+}
+
 function addPlace(name: string) {
+  checkPlaceName(name)
   DataStore.save(new Place({ name }))
 }
 
@@ -24,6 +36,7 @@ async function deletePlace(id: string) {
 }
 
 async function updatePlace(id: string, newPlaceName: string) {
+  checkPlaceName(newPlaceName)
   const original = await DataStore.query(Place, id)
 
   await DataStore.save(
