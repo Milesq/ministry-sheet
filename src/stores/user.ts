e0d33d2101d64fae14b041353300f3f67375710e
Milesq <wmilosz88@gmail.com>
2022-09-05 19:55:23 +0200

    feat: add new users programmaticly

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -1,5 +1,6 @@
 import { unimplemented } from '@/common'
 import { Auth, type CognitoUser } from '@aws-amplify/auth'
+import { API } from 'aws-amplify'
 import { defineStore } from 'pinia'
 
 interface UserState {
@@ -12,6 +13,24 @@ const DEFAULT_PASS = 'zaq1@WSX'
 
 export function transformUserName(name: string): string {
   return name.toLowerCase().replaceAll(/\s/g, '')
+}
+
+async function confirm(username: string) {
+  const apiName = 'AdminQueries'
+  const path = '/confirmUserSignUp'
+  const myInit = {
+    body: {
+      username,
+    },
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `${(await Auth.currentSession())
+        .getAccessToken()
+        .getJwtToken()}`,
+    },
+  }
+
+  return API.post(apiName, path, myInit)
 }
 
 const useUser = defineStore('user', {
@@ -55,6 +74,111 @@ const useUser = defineStore('user', {
       this.name = name
 
       await this.checkIfAdmin()
+    },
+    async asd() {
+      const users = `Oćwieja Sebastian
+Oćwieja Estera
+Martynenko Michał
+Martynenko Nadeżda
+Nadolska Alina
+Wiśniewski Miłosz
+Wiśniewski Bartosz
+Hrynczuk Jana
+Krzok Gabriela
+Kolesnikova Yevhenia
+Molchanovska Ludmiła
+Hutnik Wiktoria
+Tarnawski Jarosław
+Petryszyna Diana
+Kuchmenko Hanna
+Charczylawa Natalia
+Hohilczyn Mariana
+Grodzicki Paweł
+Grodzicka Alicja
+Melnyk Wasyl
+Melnyk Oksana
+Melnyk Dżulia
+Łukaszewicz Filip
+Podstawna Aurelia
+Sopilniak Natalia
+Bondar Elena
+Subotina Olesa
+Bodnar Julia
+Owczarz Beniamin
+Owczarz Aleksandra
+Kloska Arkadiusz
+Kloska Ilona
+Grycz Beniamin
+Grycz Joanna
+Urbaniak Zbigniew
+Urbaniak Joanna
+Goerke Adrian
+Goerke Agnieszka
+Dombrowski Mateusz
+Dombrowska Tamara
+Żurek Danuta
+Marszał Bożena
+Rozkosz Maurycy
+Rozkosz Olga
+Sobieraj Sylwia
+Schirakowski Piotr
+Schirakowska Edyta
+Gowor Mateusz
+Gowor Michelle
+Grocholewski Ryszard
+Grocholewska Irena
+Nowak Robert
+Nowak Maria
+Nowak Zofia
+Narożny Waldemar
+Narożna Sabina
+Rembacz Androna
+Nowak Mariola
+Skoropiński Jacek
+Skoropińska Żaneta
+Pawlik Wiktor
+Klimkiewicz Jarosław
+Klimkiewicz Wioletta
+Marciniak Daniel
+Marciniak Małgorzata
+Marciniak Emilia
+Pankau Agnieszka
+Kurek Ewa
+Deranek Karolina
+Kutowińska Wanda
+Malinowski Dariusz
+Kmieć Łukasz
+Kmieć Natalia
+Schirakowski Marcin
+Schirakowska Agata
+Owczarz Ewa
+Owczarz Pascal
+Kabat Witold
+Kabat Anna
+Piech Ewa
+Ziółkowski Janusz
+Ziółkowska Marlena
+Suchecki Hieronim
+Suchecka Krystyna
+Majka Ewa
+Walczak Krystyna`
+
+      for (let name of users.split('\n')) {
+        console.log(name)
+        await confirm(transformUserName(name)).catch(() => {})
+
+        name = name.split(' ').reverse().join(' ')
+        await confirm(transformUserName(name)).catch(() => {})
+
+        // await Auth.signUp({
+        //   username: transformUserName(name),
+        //   password: DEFAULT_PASS,
+        //   attributes: {
+        //     name,
+        //   },
+        // }).catch(() => console.log('name already exists', name))
+      }
+      console.log('finished')
     },
     async checkIfAdmin() {
       if (!this.isLoggedIn) {
