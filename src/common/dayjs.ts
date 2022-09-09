ca752406a3655f5b59fce503f961585ed7407c60
Milesq <wmilosz88@gmail.com>
2022-09-09 19:19:56 +0200

    feat: choose correct locale for dayjs

--- src/common/dayjs.ts
+++ src/common/dayjs.ts
@@ -1,8 +1,17 @@
 import dayjs from 'dayjs'
 import 'dayjs/locale/pl'
+import 'dayjs/locale/en'
+import 'dayjs/locale/uk'
 import weekday from 'dayjs/plugin/weekday'
+import { locale } from '@/common/userLang'
 
 dayjs.extend(weekday)
-dayjs.locale('pl')
+
+const localesMap: Record<string, string> = {
+  ukr: 'uk',
+}
+const localeKey = localesMap[locale] || locale
+
+dayjs.locale(localeKey)
 
 export default dayjs
