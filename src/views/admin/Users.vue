990cd9c3f94589feff8f87ece24fe6dbbfc9b90e
Milesq <wmilosz88@gmail.com>
2022-10-07 22:28:57 +0200

    download unapproved users

--- src/views/admin/Users.vue
+++ src/views/admin/Users.vue
@@ -1,10 +1,55 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
+import { RestAPI } from '@aws-amplify/api-rest'
+import { Auth } from '@aws-amplify/auth'
+
+interface UnconfirmedUser {
+  Username: string
+  Attributes: [
+    {
+      Name: string
+      Value: string
+    }
+  ]
+}
+
+interface ListUnconfirmedUsersResp {
+  Users: UnconfirmedUser[]
+}
 
 export default defineComponent({
   data: () => ({
     users: [] as string[],
   }),
+  async mounted() {
+    this.users = await this.listUnconfirmedUsers()
+  },
+  methods: {
+    async listUnconfirmedUsers(): Promise<string[]> {
+      const apiName = 'AdminQueries'
+      const path = '/listUnconfirmedUsers'
+      const options = {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `${(await Auth.currentSession())
+            .getAccessToken()
+            .getJwtToken()}`,
+        },
+      }
+
+      const { Users: users }: ListUnconfirmedUsersResp = await RestAPI.get(
+        apiName,
+        path,
+        options
+      )
+
+      return users.map(
+        user =>
+          user.Attributes.find(attr => attr.Name === 'name')?.Value ||
+          user.Username
+      )
+    },
+  },
 })
 </script>
 
