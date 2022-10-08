ba2df9dd353f22752ca5f90f2989636d3e523a01
Milesq <wmilosz88@gmail.com>
2022-10-08 15:32:15 +0200

    tests

--- src/common/AppSwal.ts
+++ src/common/AppSwal.ts
@@ -1,4 +1,6 @@
 import Swal from 'sweetalert2'
+const x = import('../../node_modules/@sweetalert2/theme-dark/dark.min.css')
+console.log(x)
 
 export default Swal.mixin({
   confirmButtonColor: 'var(--color-primary)',
