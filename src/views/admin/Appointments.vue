542f20817ef46f6a9c47b42490013e86775c5d18
Milesq <wmilosz88@gmail.com>
2022-10-09 14:22:00 +0200

    fix: do not try to display badge if nothing to show

--- src/views/admin/Appointments.vue
+++ src/views/admin/Appointments.vue
@@ -95,7 +95,7 @@ async function onEventClick(place: Place, date: Dayjs) {
         class="asd"
       >
         <template #label>
-          <el-badge :value="appointments.byPlaces[place.name].length">
+          <el-badge :value="appointments.byPlaces[place.name]?.length">
             {{ place.name }}
           </el-badge>
         </template>
