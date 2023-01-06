73ee630d5fe6af54b4c913ac427e9bd4c869b84a
Milesq <wmilosz88@gmail.com>
2023-01-06 08:19:10 +0100

    Revert "remove unused import"
    This reverts commit 4dc11387e7c2c4f64b4680ab0204e204e1b2e2f3.

--- src/components/ShowAppointments.vue
+++ src/components/ShowAppointments.vue
@@ -6,6 +6,7 @@ import { type Place, Appointment, PendingAppointment } from '@/models'
 import { DataStore } from '@aws-amplify/datastore'
 import { Swal, makeCalEvents, i18nFormat, type CalendarEvent } from '@/common'
 import useAppointments from '@/stores/appointments'
+import useUser from '@/stores/user'
 import Errors from '@/errors'
 
 const props = defineProps<{
@@ -15,6 +16,7 @@ const props = defineProps<{
 }>()
 
 const { t } = useI18n()
+const user = useUser()
 
 const appointments = useAppointments()
 const events = computedAsync(
