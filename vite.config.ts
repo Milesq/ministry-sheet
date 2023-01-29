fecbf736d4287d1b0b8684eeb1a57e293a1ad8b5
Milesq <wmilosz88@gmail.com>
2023-01-29 19:25:47 +0100

    vue-i18n - vite4

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import vue from '@vitejs/plugin-vue'
 import Icons from 'unplugin-icons/vite'
 import WindiCSS from 'vite-plugin-windicss'
-import vueI18n from '@intlify/vite-plugin-vue-i18n'
+import VueI18n from '@intlify/unplugin-vue-i18n/vite'
 import { resolve, dirname } from 'node:path'
 import AutoImport from 'unplugin-auto-import/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
@@ -12,7 +12,7 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   plugins: [
     vue(),
-    vueI18n({
+    VueI18n({
       include: resolve(
         dirname(fileURLToPath(import.meta.url)),
         './src/locales/**'
